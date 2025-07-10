fbda2a3d518067dbf14601e12f6b81da2391deeb
MICKAEL_MALLINGER <mallingermickael@sfr.fr>
2025-07-10 09:26:29 +0200

    update

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,11 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import BarPage from "./pages/BarPage/BarPage";
 import EventDetails from "./pages/Event/EventDetails";
+import Events from "./pages/Event/Events";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login";
 import MusicGroup from "./pages/MusicGroup/MusicGroup";
 import Register from "./pages/Register";
-import Events from "./pages/Event/Events";
 
 const router = createBrowserRouter([
   {
